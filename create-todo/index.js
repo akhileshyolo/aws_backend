56a3482889e3a30201ec8d4954aff134bb885f1c
Akhilesh Yadav1 <Akhilesh.Yadav1@morningstar.com>
2021-03-24 23:55:38 +0530

    push working commit

--- create-todo/index.js
+++ create-todo/index.js
@@ -1,5 +1,39 @@
 // TODO: Add code for creating todos
+var AWS = require("aws-sdk");
+const { v4: uuidv4 } = require('uuid');
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function (event, context) {
-  return "Create todo";
+exports.handler = async(event, context, callback) => {
+  const data = JSON.parse(event.body);
+  const id = uuidv4();
+  let resultStatus;
+  try {
+    const params = {
+      TableName: "todo-table",
+      Item: {
+        "pk": id,
+        "title": data.title,
+        "task": data.task
+      }
+    };
+    resultStatus = await DynamoDB.put(params).promise()
+      .then(function(data) {
+        return {id}
+      })
+      .catch(function(err) {
+        console.log({ err });
+        return 400;
+      });
+  }
+  catch (err) {
+    resultStatus = 400;
+  }
+  
+  callback(null, {
+    statusCode: resultStatus === 400 ? 400 : 200,
+    body: JSON.stringify({
+      response: resultStatus
+    })
+  });
+
 };
--- read-todo/index.js
+++ read-todo/index.js
@@ -1,5 +1,49 @@
-// TODO: Add code for reading todos
+const AWS = require("aws-sdk");
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function (event, context) {
-  return "Create todo";
+exports.handler = async function(event, context, callback) {
+    console.log(event);
+    let resultStatus;
+    let resData = "";
+    try {
+        const params = {
+            TableName: "todo-table",
+            Key: {
+                "pk": event.queryStringParameters.id
+            }
+        };
+
+        // Call DynamoDB to read the item from the table
+        resData = await DynamoDB.get(params).promise()
+            .then(function(data) {
+                resultStatus = 200;
+                return data.Item;
+            })
+            .catch(function(err) {
+                console.log({ err });
+                resultStatus = 400;
+                return;
+            });
+    }
+    catch (err) {
+        resultStatus = 400;
+    }
+
+    if (resData) {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                response: resData
+            })
+        });
+    }
+    else {
+        callback(null, {
+            statusCode: 400,
+            body: JSON.stringify({
+                response: resData
+            })
+        });
+    }
+
 };
